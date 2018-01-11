2a9628a7bd0b11554a34d9ce65860adf304f016d
Wazbat <Wazbat@169.254.191.62>
2018-01-11 09:23:53 +0100

    Parte 3 completo

--- src/warren/pongfx/Main.java
+++ src/warren/pongfx/Main.java
@@ -1,19 +1,24 @@
 package warren.pongfx;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 300, 250);
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 600, 400);
+        scene.setFill(Color.BLACK);
         primaryStage.setTitle("PongFX");
         primaryStage.setScene(scene);
         primaryStage.show();
-        primaryStage.setFullScreen(true);
+        Circle circleBall = new Circle(10, 30, 7);
+        circleBall.setFill(Color.WHITE);
+        root.getChildren().add(circleBall);            
     }   
     
 }
