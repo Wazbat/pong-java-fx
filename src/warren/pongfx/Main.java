2e32b9a19e6e3775ae5b60b72f497111391a4c11
Wazbat <Wazbat@169.254.191.62>
2018-01-12 14:49:12 +0100

    Parte 4 completo

--- src/warren/pongfx/Main.java
+++ src/warren/pongfx/Main.java
@@ -1,24 +1,68 @@
 package warren.pongfx;
+import static java.time.Clock.system;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    //Variables
+    int velbolax = 3;
+    int velbolay = 3;
+    int bolaposx = 20;
+    int bolaposy = 10;
+    int bolagrande = 7;
+    int barra1posx = 5;
+    int barra1posy = 50;
     
+    int alturaventana=400;
+    int anchuraventana=600;
     @Override
     public void start(Stage primaryStage) {
         Pane root = new Pane();
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, anchuraventana, alturaventana);
         scene.setFill(Color.BLACK);
         primaryStage.setTitle("PongFX");
         primaryStage.setScene(scene);
         primaryStage.show();
-        Circle circleBall = new Circle(10, 30, 7);
-        circleBall.setFill(Color.WHITE);
-        root.getChildren().add(circleBall);            
+        
+       
+        
+        Rectangle bola = new Rectangle(bolaposx, bolaposy,7, 7);
+        bola.setFill(Color.WHITE);
+        Rectangle player1 = new Rectangle(barra1posx, barra1posy, 10, 40);
+        player1.setFill(Color.WHITE);
+        root.getChildren().addAll(bola,player1);
+        AnimationTimer animacion = new AnimationTimer() {
+            @Override
+            public void handle(long now){
+                bolaposx+=velbolax;
+                bolaposy+=velbolay;
+                if (bolaposx >=anchuraventana-bolagrande){
+                    velbolax=-3;
+                }
+                if (bolaposx <0){
+                    velbolax=3;
+                }
+                if (bolaposy >= alturaventana-bolagrande){
+                    velbolay=-3;
+                }
+                if (bolaposy <0){
+                    velbolay=3;
+                }
+                bola.setX(bolaposx);
+                bola.setY(bolaposy);
+                System.out.println(bolaposy);
+            };
+        };
+        animacion.start();
+        }
+       
     }   
+   
     
-}
+
+
